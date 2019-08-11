db74e02a53c1fe12d55aa5a2f883b9b9c1cebc8c
Aleksandar Markovski <aleksandar@gsix.me>
2019-08-11 22:28:04 +0200

    Add quit after EOF 0 seconds

--- share.sh
+++ share.sh
@@ -6,17 +6,14 @@ PID=$$
 touch "/tmp/$SCREEN_TMP_FILE"
 chmod 640 "/tmp/$SCREEN_TMP_FILE"
 
-#fpfunction &
-#child_pid=$!
-
 ( while true; do 
 	echo "$(cat /tmp/$SCREEN_TMP_FILE | /bin/bash ansi2html.sh --inline )" | 
-	cat screen.headers - | nc -l -N 8001 >> /dev/null; 
+	cat screen.headers - | nc -l -q 0 8001 >> /dev/null; 
 done ) &
 
-( while true; do cat index.http | nc -l -N 8000 >> /dev/null; done ) &
+( while true; do cat index.http - | nc -l -q 0 8000 >> /dev/null; done ) &
 
 
 echo "Screen share available on http://localhost:8000"
-echo "Kill server with command: kill 9 $PID"
+echo "Kill server with command: kill $PID"
 script -f -q "/tmp/$SCREEN_TMP_FILE"
